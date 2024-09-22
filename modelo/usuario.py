6fb226c0c0ded829697ffc20fa99ddc87284a76d
alexmillano <159828484+alexmillano@users.noreply.github.com>
2024-09-22 13:18:32 -0300

    Clase usuario

---
+++ modelo/usuario.py
@@ -0,0 +1,14 @@
+class Usuario:
+    def __init__(self, id, nombre, apellido, fecharegistro, correo, telefono, usuario, clave, seguidores, seguidos, foto, biografia):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.fecharegistro = fecharegistro
+        self.correo = correo
+        self.telefono = telefono
+        self.usuario = usuario
+        self.clave = clave
+        self.seguidores = seguidores
+        self.seguidos = seguidos
+        self.foto = foto
+        self.biografia = biografia
